f8e86f13dfb0d6eb3ede5f4ce4dcdf930b4ce153
BlueKitsune狐 <116144344+bluekitsune-sad@users.noreply.github.com>
2024-09-28 20:04:45 +0500

    Update calculator.py

--- challenge/calculator.py
+++ challenge/calculator.py
@@ -1,5 +1,5 @@
 class Calculator:
-    def __init__(self, *exc_types):
+    def __init__(self, *exc_types):  # in this we are using a method called contect manager in which we close the software according to the argument in the exit method
         self.exc_types = exc_types
     def __enter__(self):
         return self
